43a69fc7df06682270cf70a9db914cd49fd70c67
drkcn94 <derekm.chanchan@gmail.com>
2023-04-29 10:59:49 -0400

    Begin Implementation and Refactoring of Roster Class

--- src/main/java/com/badminton/shuttlestats/controller/RosterController.java
+++ src/main/java/com/badminton/shuttlestats/controller/RosterController.java
@@ -4,8 +4,11 @@ import com.badminton.shuttlestats.model.Player;
 import com.badminton.shuttlestats.model.Roster;
 import com.badminton.shuttlestats.services.RosterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clubs/{clubId}/roster")
@@ -15,13 +18,15 @@ public class RosterController {
     private RosterService rosterService;
 
     @GetMapping("/")
-    public ResponseEntity<Player> getPlayersOfClub() {
-
+    public ResponseEntity<List<Player>> getPlayersOfClub() {
+        return new ResponseEntity<List<Player>>(HttpStatus.OK);
     }
 
     @PostMapping("/addPlayer")
     public ResponseEntity<Roster> addPlayerToRoster(@PathVariable String clubId, Player player) {
-
+        return new ResponseEntity<Roster>(HttpStatus.OK);
     }
+
+
 
 }
