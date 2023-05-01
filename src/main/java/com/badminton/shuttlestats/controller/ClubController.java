bff115ea0612c799f5329b3bad11aa6efddb3937
drkcn94 <derekm.chanchan@gmail.com>
2023-04-30 22:11:28 -0400

    Added files for implementing Session Functions

--- src/main/java/com/badminton/shuttlestats/controller/ClubController.java
+++ src/main/java/com/badminton/shuttlestats/controller/ClubController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/clubs")
+@RequestMapping("/club")
 public class ClubController {
     @Autowired
     private ClubService clubService;
--- src/main/java/com/badminton/shuttlestats/controller/SessionController.java
+++ src/main/java/com/badminton/shuttlestats/controller/SessionController.java
@@ -1,4 +1,34 @@
 package com.badminton.shuttlestats.controller;
 
+import com.badminton.shuttlestats.model.Session;
+import com.badminton.shuttlestats.services.RosterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/club={clubId}/session")
 public class SessionController {
+
+    @Autowired
+    private RosterService rosterService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Session>> getSessionsOfClub() {
+        return new ResponseEntity<List<Session>>(HttpStatus.OK);
+    }
+
+    @PostMapping("/addSession")
+    public ResponseEntity<Session> addSessionToClub() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteSession")
+    public ResponseEntity<Void> deleteSession() {
+        return ResponseEntity.noContent().build();
+    }
+
 }
