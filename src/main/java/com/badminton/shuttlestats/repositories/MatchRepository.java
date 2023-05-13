da943323029853de94bab09d137aee192a5fc688
drkcn94 <derekm.chanchan@gmail.com>
2023-05-12 23:40:29 -0400

    Remove unnecessary Import in MatchRepository

--- src/main/java/com/badminton/shuttlestats/repositories/MatchRepository.java
+++ src/main/java/com/badminton/shuttlestats/repositories/MatchRepository.java
@@ -1,7 +1,6 @@
 package com.badminton.shuttlestats.repositories;
 
 import com.badminton.shuttlestats.model.Match;
-import com.badminton.shuttlestats.model.keys.MatchId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
